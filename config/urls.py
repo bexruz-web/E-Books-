11306f91f1b43c7cd27c0225a1a157f9dc41c58c
bexruz <bexruzasqarov08@gmail.com>
2025-02-15 20:21:54 +0500

    startproject full completed

---
+++ config/urls.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API E-Book",
+        default_version='v1',
+        description="E-Book API",
+        terms_of_service="https://bexruzwebb.pythonanywhere.com/",
+        contact=openapi.Contact(email="bexruzasqarov08@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('book.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
