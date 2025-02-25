fec1e49a6d75c71719808f859794eee9f2f7d505
bexruz <bexruzasqarov08@gmail.com>
2025-02-25 22:52:48 +0500

    custom permissions

---
+++ book/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow, GET, HEAD, or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner
+        return obj.owner == request.user
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Custom permissions to only allow staff members to edit or delete books
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
