e74caff8f53dfb9bdf931b1ad2f1f3caaee5de3d
bexruz <bexruzasqarov08@gmail.com>
2025-02-25 22:45:52 +0500

    Add place_order and replenish_stock

---
+++ book/services/__init__.py
@@ -0,0 +1,3 @@
+from .replenish_stock import *
+from .book_view_history import *
+from .flash_sale import *
---
+++ book/services/replenish_stock.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.contrib.admin.views.decorators import staff_member_required
+from book.models import Book
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+@swagger_auto_schema(operation_description="Admin replenishes stock for a book")
+def admin_replenish_stock(request, book_id, amount):
+    try:
+        book = Book.objects.get(id=book_id)
+        book.increase_stock(amount)
+
+        return JsonResponse({'status': 'success', 'message': f'Successfully replenished stock by {amount}'})
+
+    except Book.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Book does not exist'}, status=400)
+
+    except ValueError:
+        return HttpResponseBadRequest('Invalid input!')
